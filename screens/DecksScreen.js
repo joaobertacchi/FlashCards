fe47b4fd7479171afc50c688d97dce38a2a4d9e3
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2019-05-21 23:56:01 -0300

    feat: add decks list placeholder

--- screens/DecksScreen.js
+++ screens/DecksScreen.js
@@ -1,8 +1,7 @@
 // @flow
 
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { FlatList, StyleSheet, Text } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,17 +11,54 @@ const styles = StyleSheet.create({
   },
 });
 
+const decks = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces',
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event',
+      },
+    ],
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer:
+          'The combination of a function and the lexical environment within which that function was declared.',
+      },
+    ],
+  },
+};
+
 export default class DecksScreen extends React.Component<*, *> {
   static navigationOptions = {
-    title: 'Decks',
+    title: 'All decks',
   };
+
+  renderItem = ({ item }) => {
+    const deckSize = item.questions.length;
+    return (
+      <Text>{`${item.title} - ${deckSize} card${deckSize > 1 ? 's' : ''}`}</Text>
+    );
+  };
+
+  getKey = item => item.title;
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* TODO: list of decks */}
-        <ExpoLinksView />
-      </ScrollView>
+      <FlatList
+        style={styles.container}
+        data={Object.values(decks)}
+        renderItem={this.renderItem}
+        keyExtractor={this.getKey}
+      />
     );
   }
 }
