78cc8c22f8af6922e2b84917b114bf82239d4783
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2019-05-23 00:51:35 -0300

    chore: configure jest to work with async-storage

---
+++ jestSetupFile.js
@@ -0,0 +1,4 @@
+// @flow
+import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
