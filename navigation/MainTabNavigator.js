e708b9a9283ab4a3dec9d3f508a9ae88ae58af41
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2019-06-06 00:06:41 -0300

    refactor: remove Settings screen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -2,14 +2,10 @@
 
 import React from 'react';
 import { Platform } from 'react-native';
-import {
-  createStackNavigator,
-  createBottomTabNavigator,
-} from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import AboutScreen from '../screens/AboutScreen';
 import TabBarIcon from '../components/TabBarIcon';
-import SettingsScreen from '../screens/SettingsScreen';
 import DecksScreen from '../screens/DecksScreen';
 import DeckScreen from '../screens/DeckScreen';
 import NewDeckScreen from '../screens/NewDeckScreen';
@@ -60,17 +56,6 @@ AboutStack.navigationOptions = {
           : 'md-information-circle'
       }
     />
-  ),
-};
-
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
   ),
 };
 
