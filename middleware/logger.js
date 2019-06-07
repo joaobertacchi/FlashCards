f2356708bab14d86abc497a32b9037b1a7e0b725
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2019-06-06 23:48:24 -0300

    feat: remove log middleware from non-dev builds

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,12 +1,15 @@
 // @flow
 
 const logger = (store: Object): Function => (next: Function): Function => (action: Object): any => {
-  console.group(action.type);
-  console.log('The action is: ', action);
-  const returnValue = next(action);
-  console.log('The new state is: ', store.getState());
-  console.groupEnd();
-  return returnValue;
+  if (__DEV__) {
+    console.group(action.type);
+    console.log('The action is: ', action);
+    const returnValue = next(action);
+    console.log('The new state is: ', store.getState());
+    console.groupEnd();
+    return returnValue;
+  }
+  return next(action);
 };
 
 export default logger;
